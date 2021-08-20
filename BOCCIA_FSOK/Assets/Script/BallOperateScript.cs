bbeb8e2588c625d8b44f00c25ba471036c595727
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-08-20 14:56:40 +0900

    ファイルの追加

---
+++ BOCCIA_FSOK/Assets/Script/BallOperateScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallOperateScript : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //コンポーネントを取得
+        m_rigidbody = GetComponent<Rigidbody>();
+        AddSpeed(new Vector3(0.0f,0.0f,1500.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// リジッドボディに速度を加算
+    /// </summary>
+    /// <param name="speed">加算する速度</param>
+    void AddSpeed(Vector3 speed)
+    {
+        //速度を加算
+        m_rigidbody.AddForce(speed);
+        //m_rigidbody.velocity += speed;
+    }
+}
