d20696d2558520c63cc3341e6edb6035e2decaf9
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-08-26 13:54:44 +0900

    カメラ切り替え処理の実装。
    一人称と上から見た図の切り替えは出来た。

---
+++ BOCCIA_FSOK/Assets/Camera/SwitchActive.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchActive : MonoBehaviour
+{
+    public bool m_isActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(m_isActive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// アクティブ状態を切り替える。
+    /// </summary>
+    public void SwitchObjectActive()
+    {
+        m_isActive = true ^ m_isActive;
+        this.gameObject.SetActive(m_isActive);
+    }
+}
