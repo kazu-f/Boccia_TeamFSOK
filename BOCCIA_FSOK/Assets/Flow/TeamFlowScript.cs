06345b777f3d089e8bcadcb0ba8dc150c2cc2b71
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-09-02 09:57:59 +0900

    TeamFlowScriptの中身で初めにJackボールを投げるようにした Jackボールが準備されたら次に赤チームが投げるようにしました

--- BOCCIA_FSOK/Assets/Flow/TeamFlowScript.cs
+++ BOCCIA_FSOK/Assets/Flow/TeamFlowScript.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class TeamFlowScript : MonoBehaviour
 {
     public Team m_team = Team.Num;
+    private BallFlowScript m_BallFlow;
     // Start is called before the first frame update
     void Start()
     {
-        m_team = Team.Red;
+        m_team = Team.Jack;
+        m_BallFlow = GetComponent<BallFlowScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //次に投げるチームによって処理を変える
+        switch(m_team)
+        {
+            //ジャックボールを投げるとき
+            case Team.Jack:
+                if(m_BallFlow.IsPreparedJack())
+                {
+                    //ジャックボールが準備されたら次は赤チームが投げる
+                    m_team = Team.Red;
+                }
+                break;
+
+            case Team.Red:
+                break;
+
+            case Team.Blue:
+                break;
+
+            default:
+                Debug.Log("エラー：次に投げるチームが決まっていません");
+                break;
+        }
     }
 
     public Team GetNowTeam()
