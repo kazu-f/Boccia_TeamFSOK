892633c3a566664986cdbfca434437ab1ecac302
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-10-01 10:26:09 +0900

    Addし忘れをプッシュ

---
+++ BOCCIA_FSOK/Assets/Court/BallKillAreaScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallKillAreaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<BallOperateScript>().OutsideVenue();
+    }
+
+}
