7888954e3c8c1f2c22f0817fad402a9f0995ec7a
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-11-29 02:42:24 +0900

    通信で投げるチームが切り替わった際に効果音が鳴るのを修正。

--- BOCCIA_FSOK/Assets/Ball/BallBoundSound.cs
+++ BOCCIA_FSOK/Assets/Ball/BallBoundSound.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class BallBoundSound : MonoBehaviour
 {
     public AudioSource boundSE;
+    private IBallScript ballScript;
     // Start is called before the first frame update
     void Start()
     {
+        ballScript = this.gameObject.GetComponent<IBallScript>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class BallBoundSound : MonoBehaviour
     /// <param name="collision"></param>
     private void OnCollisionEnter(Collision collision)
     {
+        if (!ballScript.GetIsThrow()) return;
         if(collision.gameObject.tag == "Court")
         {
             boundSE.Play();
